f5a16eb43f81d28a641e04426d52ff6ef11b620d
fernandoperez-48 <fernandrperez@gmail.com>
2026-03-07 18:36:52 -0300

    Primer commit

---
+++ back/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT} en modo ${process.env.NODE_ENV}`);
+});
